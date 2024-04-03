53183e3bb1d0b3be3459241c8688594a46059177
Hernandez Suarez Zuzzet <zS22017025@estudiantes.uv.mx>
2024-04-03 16:02:08 -0600

    ñaaaaa

--- construccion/java/helloworld/src/main/java/mx/zhs/helloworld/HelloworldApplication.java
+++ construccion/java/helloworld/src/main/java/mx/zhs/helloworld/HelloworldApplication.java
@@ -1,13 +1,19 @@
 package mx.zhs.helloworld;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloworldApplication {
+public class HelloworldApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("My first Spring...");
 	}
 
 }
