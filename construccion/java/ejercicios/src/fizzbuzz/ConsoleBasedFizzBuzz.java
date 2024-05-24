7955975bb615ea6d3e5573ab73c2d6d79ae5c489
Hernandez Suarez Zuzzet <zS22017025@estudiantes.uv.mx>
2024-05-23 19:15:06 -0600

    odio todo

--- construccion/java/ejercicios/src/fizzbuzz/ConsoleBasedFizzBuzz.java
+++ construccion/java/ejercicios/src/fizzbuzz/ConsoleBasedFizzBuzz.java
@@ -3,17 +3,49 @@ package fizzbuzz;
 public class ConsoleBasedFizzBuzz implements FizzBuzz {
     @Override
     public void print(int from, int to) {
+
         while (from <= to) {
+
             if (from % 3 == 0 && from % 5 == 0) {
+
                 System.out.println("FizzBuzz");
+
             } else if (from%3==0) {
+
                 System.out.println("Fizz");
+
             } else if (from%5==0) {
+
                 System.out.println("Buzz");
+
+            } else {
+
+                System.out.println(from);
+
+            }
+
+            from++;
+        }
+    }
+
+    public void print2(int from, int to) {
+        int[] numbers = {15,5,3};
+
+        while (from <= to) {
+            if (esMultiplo(from, numbers[0])) {
+                System.out.println("FizzBuzz");
+            } else if (esMultiplo(from, numbers[1])) {
+                System.out.println("Buzz");
+            } else if (esMultiplo(from, numbers[2])) {
+                System.out.println("Fizz");
             } else {
                 System.out.println(from);
             }
             from++;
         }
+    }
+
+    private boolean esMultiplo(int num, int multiplo) {
+        return num%multiplo==0;
     }
 }
--- construccion/java/ejercicios/src/fizzbuzz/Main.java
+++ construccion/java/ejercicios/src/fizzbuzz/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         ConsoleBasedFizzBuzz c = new ConsoleBasedFizzBuzz();
 
-        c.print(1,100);
+        c.print(1,10);
+        c.print2(1,10);
     }
 }
